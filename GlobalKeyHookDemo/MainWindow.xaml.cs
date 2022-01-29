7f1e76c4f90f7c2a55fc9050e828242f77173895
mybigfriendjo <mybigfriendjo@users.noreply.github.com>
2022-01-29 12:38:57 +0100

    initial commit

---
+++ GlobalKeyHookDemo/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Forms;
+using GlobalKeyHookDemo.helpers;
+
+namespace GlobalKeyHookDemo
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly KeyboardHook _hook = new KeyboardHook();
+
+        private readonly List<string> _wisdom = new List<string>
+        {
+            "The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela",
+            "The way to get started is to quit talking and begin doing. -Walt Disney",
+            "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking. -Steve Jobs",
+            "If life were predictable it would cease to be life, and be without flavor. -Eleanor Roosevelt",
+            "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough. -Oprah Winfrey",
+            "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success. -James Cameron",
+            "Life is what happens when you're busy making other plans. -John Lennon"
+        };
+
+        private readonly Random _rand = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _hook.KeyPressed += hook_KeyPressed;
+            _hook.RegisterHotKey(ModifierKeys.None, Keys.F8);
+        }
+
+        private void hook_KeyPressed(object sender, KeyPressedEventArgs e) { txtQuote.Text = _wisdom[_rand.Next(_wisdom.Count)]; }
+
+        private void MainWindow_OnClosing(object sender, CancelEventArgs e) { _hook.Dispose(); }
+    }
+}
---
+++ GlobalKeyHookDemo/helpers/KeyboardHook.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
+using MessageBox = System.Windows.MessageBox;
+
+namespace GlobalKeyHookDemo.helpers
+{
+    public sealed class KeyboardHook
+    {
+        private readonly Window _window = new Window();
+        private int _currentId;
+
+        public KeyboardHook()
+        {
+            // register the event of the inner native window.
+            _window.KeyPressed += delegate(object sender, KeyPressedEventArgs args) { KeyPressed?.Invoke(this, args); };
+        }
+
+        // Registers a hot key with Windows.
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        // Unregisters the hot key with Windows.
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        /// <summary>
+        ///     Registers a hot key in the system.
+        /// </summary>
+        /// <param name="modifier">The modifiers that are associated with the hot key.</param>
+        /// <param name="key">The key itself that is associated with the hot key.</param>
+        public void RegisterHotKey(ModifierKeys modifier, Keys key)
+        {
+            // increment the counter.
+            _currentId = _currentId + 1;
+
+            // register the hot key.
+            if (!RegisterHotKey(_window.Handle, _currentId, (uint)modifier, (uint)key))
+            {
+                MessageBox.Show("Der Hotkey konnte nicht gesetzt werden!\r\nEvtl. funktioniert der Aufruf via Hotkey nicht.",
+                                "Hotkey Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                _currentId = _currentId - 1;
+                // throw new InvalidOperationException("Couldn’t register the hot key.");
+            }
+        }
+
+        /// <summary>
+        ///     A hot key has been pressed.
+        /// </summary>
+        public event EventHandler<KeyPressedEventArgs> KeyPressed;
+
+        /// <summary>
+        ///     Represents the window that is used internally to get the messages.
+        /// </summary>
+        private class Window : NativeWindow, IDisposable
+        {
+            private const int WM_HOTKEY = 0x0312;
+
+            public Window()
+            {
+                // create the handle for the window.
+                // ReSharper disable once VirtualMemberCallInContructor
+                CreateHandle(new CreateParams());
+            }
+
+            #region IDisposable Members
+
+            public void Dispose() { DestroyHandle(); }
+
+            #endregion
+
+            /// <summary>
+            ///     Overridden to get the notifications.
+            /// </summary>
+            /// <param name="m"></param>
+            protected override void WndProc(ref Message m)
+            {
+                base.WndProc(ref m);
+
+                // check if we got a hot key pressed.
+                if (m.Msg == WM_HOTKEY)
+                {
+                    // get the keys.
+                    Keys key = (Keys)(((int)m.LParam >> 16) & 0xFFFF);
+                    ModifierKeys modifier = (ModifierKeys)((int)m.LParam & 0xFFFF);
+
+                    // invoke the event to notify the parent.
+                    KeyPressed?.Invoke(this, new KeyPressedEventArgs(modifier, key));
+                }
+            }
+
+            public event EventHandler<KeyPressedEventArgs> KeyPressed;
+        }
+
+        #region IDisposable Members
+
+        public void Reset()
+        {
+            // unregister all the registered hot keys.
+            for (int i = _currentId; i > 0; i--)
+            {
+                UnregisterHotKey(_window.Handle, i);
+            }
+        }
+
+        public void Dispose()
+        {
+            Reset();
+
+            // dispose the inner native window.
+            _window.Dispose();
+        }
+
+        #endregion
+    }
+
+    /// <summary>
+    ///     Event Args for the event that is fired after the hot key has been pressed.
+    /// </summary>
+    public class KeyPressedEventArgs : EventArgs
+    {
+        public Keys Key { get; }
+        public ModifierKeys Modifier { get; }
+
+        internal KeyPressedEventArgs(ModifierKeys modifier, Keys key)
+        {
+            Modifier = modifier;
+            Key = key;
+        }
+    }
+
+    /// <summary>
+    ///     The enumeration of possible modifiers.
+    /// </summary>
+    [Flags]
+    public enum ModifierKeys : uint
+    {
+        None = 0,
+        Alt = 1,
+        Control = 2,
+        Shift = 4,
+        Win = 8
+    }
+}
